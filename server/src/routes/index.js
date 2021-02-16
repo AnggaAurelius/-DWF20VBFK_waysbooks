5a145d8efc1b8611557719a59caf10456f0e0782
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-16 10:13:42 +0700

    register and login API

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const { getUsers } = require("../controllers/users");
 
+const { register, login } = require("../controllers/auth");
+
+// auth
+router.post("/register", register);
+router.post("/login", login);
+
+// user
 router.get("/users", getUsers);
 
 module.exports = router;
