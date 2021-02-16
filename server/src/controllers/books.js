af5146910a7f5cf1a43eea2f40ea7e6f4ed6c9c1
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-16 20:36:49 +0700

    add book
    create table
    install multer

---
+++ server/src/controllers/books.js
@@ -0,0 +1,81 @@
+const { Book } = require("../../models");
+
+exports.getBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll();
+
+    res.send({
+      status: "success",
+      data: {
+        books,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+exports.getBooksById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+    if (!book) {
+      return res.send({
+        status: "success",
+        message: `Book with id ${id} Not Existed`,
+      });
+    }
+
+    res.send({
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+exports.addBook = async (req, res) => {
+  const { files } = req;
+
+  const { title, publicationDate, pages, author, isbn, about } = req.body;
+
+  try {
+    const book = await Book.create({
+      title,
+      publicationDate,
+      pages,
+      author,
+      isbn,
+      about,
+      thumbnail: files.thumbnail[0].filename,
+      bookAttachment: files.bookAttachment[0].filename,
+    });
+
+    res.send({
+      message: "Posts Successfully Created",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,16 +2,24 @@ const express = require("express");
 const router = express.Router();
 const { authenticated } = require("../middlewares/auth");
 const { isAdmin } = require("../middlewares/checkRole");
+const { uploadBook } = require("../middlewares/uploadBook");
 
 const { getUsers } = require("../controllers/users");
 
 const { register, login } = require("../controllers/auth");
 
+const { getBooks, getBooksById, addBook } = require("../controllers/books");
+
 // auth
 router.post("/register", register);
 router.post("/login", login);
 
-// user
+// users
 router.get("/users", authenticated, isAdmin, getUsers);
+
+// books
+router.get("/books", getBooks);
+router.get("/book/:id", getBooksById);
+router.post("/upload-book", uploadBook("thumbnail", "bookAttachment"), addBook);
 
 module.exports = router;
