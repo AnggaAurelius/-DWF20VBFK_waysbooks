dbe6bc7396c180c5550ce7d83a6f99ac80ffa746
Angga Afrianto <anggaaurelius@gmail.com>
2021-02-16 14:16:38 +0700

    integration register

--- client/src/pages/LandingPage/register/index.jsx
+++ client/src/pages/LandingPage/register/index.jsx
@@ -1,9 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { AppContext } from "../../../component/context/Global";
 import { Form } from "react-bootstrap";
+import { API, setAuthToken } from "../../../config/axios";
 
 const Register = () => {
+  const history = useHistory();
+  const [state, dispatch] = useContext(AppContext);
+
   const [signUpModal, setSignUpModal] = useState(false);
   const setOverlay = () => setSignUpModal(false);
+
+  const [formDataRegister, setFormDataRegister] = useState({
+    email: "",
+    password: "",
+    fullname: "",
+  });
+  const { email, password, fullname } = formDataRegister;
+  const handleChangeRegister = (e) => {
+    setFormDataRegister({
+      ...formDataRegister,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const registerSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = JSON.stringify({
+        email: email,
+        password: password,
+        fullName: fullname,
+      });
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const user = await API.post("/register", body, config);
+
+      dispatch({
+        type: "LOGIN_SUKSES",
+        payload: user.data.data.user,
+      });
+
+      setAuthToken(user.data.data.user.token);
+
+      history.push("/beranda");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="container">
       <button
@@ -15,15 +64,15 @@ const Register = () => {
       <div className={`pl-5 pr-5 Modal ${signUpModal ? "Show" : ""}`}>
         <h3 className="mgtop pt-4 bold">Sign Up</h3>
         <br />
-        <Form>
+        <Form onSubmit={(e) => registerSubmit(e)}>
           <Form.Group controlId="formBasicEmail" className=" form">
             <Form.Control
               className="p-3 form"
               type="email"
               placeholder="Email"
-              name="email2"
-              //   value={email2}
-              //   onChange={(e) => handleChangeRegister(e)}
+              name="email"
+              value={email}
+              onChange={(e) => handleChangeRegister(e)}
               required
             />
           </Form.Group>
@@ -32,9 +81,9 @@ const Register = () => {
               className="form"
               type="password"
               placeholder="Password"
-              name="password2"
-              //   value={password2}
-              //   onChange={(e) => handleChangeRegister(e)}
+              name="password"
+              value={password}
+              onChange={(e) => handleChangeRegister(e)}
               required
             />
           </Form.Group>
@@ -44,8 +93,8 @@ const Register = () => {
               type="text"
               placeholder="Full Name"
               name="fullname"
-              //   value={fullname}
-              //   onChange={(e) => handleChangeRegister(e)}
+              value={fullname}
+              onChange={(e) => handleChangeRegister(e)}
               required
             />
           </Form.Group>
